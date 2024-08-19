b9854dd588b90a190e0fb31ac2c3ada0b0c9e3d5
MT <burnerforthee@proton.me>
2024-08-20 00:47:21 +0700

    Implement a GoRouter singleton

--- lib/helpers/routers.dart
+++ lib/helpers/routers.dart
@@ -2,23 +2,27 @@ import 'package:go_router/go_router.dart';
 import 'package:openViewF1/views/home.dart';
 import 'package:openViewF1/views/session_detail.dart';
 
-late GoRouter goRouter;
+class Routers {
+  Routers._();
 
-GoRouter initGoRouter() {
-  return goRouter = GoRouter(
-    routes: <RouteBase>[
-      GoRoute(
-        path: '/',
-        builder: (context, state) => const Home(),
-        routes: <RouteBase>[
-          GoRoute(
-            path: 'session/:year/:country',
-            builder: (context, state) => SessionDetail(
-              queryParams: state.extra!,
+  static final goRouterInstance = Routers._();
+
+  GoRouter initGoRouter() {
+    return GoRouter(
+      routes: <RouteBase>[
+        GoRoute(
+          path: '/',
+          builder: (context, state) => const Home(),
+          routes: <RouteBase>[
+            GoRoute(
+              path: 'session/:year/:country',
+              builder: (context, state) => SessionDetail(
+                queryParams: state.extra!,
+              ),
             ),
-          ),
-        ],
-      ),
-    ],
-  );
+          ],
+        ),
+      ],
+    );
+  }
 }
