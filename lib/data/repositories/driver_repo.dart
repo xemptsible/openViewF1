04c956fbedf0c0578e1bce4cd3ba4f3ea3e0726c
MT <burnerforthee@proton.me>
2024-08-23 19:45:04 +0700

    Add driver data

---
+++ lib/data/repositories/driver_repo.dart
@@ -0,0 +1,15 @@
+import 'package:openViewF1/data/repositories/.repository.dart';
+
+class DriverRepo implements Repository {
+  @override
+  Future<List> getAll() async {
+    // TODO: implement getAll
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List> getWithFilter({queryParams}) async {
+    // TODO: implement getWithFilter
+    throw UnimplementedError();
+  }
+}
